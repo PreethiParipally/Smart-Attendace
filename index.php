a601629dc3fe6d11d8468fd3c4db677f8a7f0fb5
Preethi <preethiparipally5819@gmail.com>
2021-07-22 17:48:14 +0530

    login button reset

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 
@@ -26,16 +27,18 @@
               <div class="wrapper">
                 <div class="col-lg-12 login-form">
                     <div class="col-lg-12 login-form">
-                        <form name="form" METHOD ="POST" action="" class="frm">
+                        <form name="form" METHOD ="POST" action="index.php" class="frm">
                             <div class="form-group">
                               <label class="form-control-label"><h5>USER NAME :</h5></label>
-                                <input type="text" class="form-control" name="username"  maxlength=10 >
+                                <input type="text" class="form-control" name="username"  maxlength=10 required>
                             </div>
                             <div class="form-group">
                               <label class="form-control-label"><h5>PASSWORD :</h5></label>
-                                <input type="password" class="form-control"  name="password" >
+                                <input type="password" class="form-control"  name="password" required>
                             </div>
-
+                            <div class="col-lg-12 loginbttm">
+                                <p><a href="username.php">Forgot password?</a></p>
+                            </div>
                             <div class="col-lg-12 loginbttm">
                                 <div class="col-lg-6 login-btm login-text">
                                     <!-- Error Message -->
@@ -44,9 +47,7 @@
                                     <button type="submit"  name="Login" class="btn btn-outline-primary">LOGIN</button>
                                 </div>
                             </div>
-                            <div class="col-lg-12 loginbttm">
-                                <p><a href="username.php">Forgot password?</a></p>
-                            </div>
+                            
                         </form>
                     </div>
                 </div>
@@ -59,3 +60,45 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
+<?php
+include('sanitize.php');
+include("dbcon2.php");
+if (isset($_POST['Login'])) {
+    $username = sanitize($_POST['username']);
+    $password = sanitize($_POST['password']);
+    $con = mysqli_connect("localhost", "root", "", "attendance");
+    $sql = "SELECT id,tag FROM user WHERE username = '$username' and password = '$password' ";
+    $result = mysqli_query($con, $sql);
+    if (!$result) {
+        printf("Error: %s\n", mysqli_error($con));
+        exit();
+    }
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $count = mysqli_num_rows($result);
+    if ($count == 1) {
+        // echo"Hello!";
+        // print $row['tag'];
+        // $xt=$row['tag'] == 'student';
+        // echo $xt;
+        if ($row['tag'] == 'student') {
+            session_start();
+            $_SESSION['state'] = 'active';
+            $_SESSION['name'] = $username;
+            header("Location: homestudent.php");
+        } else if($row['tag'] == 'teacher'){ //modified
+            session_start();
+            $_SESSION['state'] = 'active';
+            $_SESSION['name'] = $username;
+            header("Location: hometeacher.php");
+        }
+        else {
+            session_start();
+            $_SESSION['state'] = 'active';
+            $_SESSION['name'] = $username;
+            header("Location: home.php");
+        }
+    } else {
+        echo "<br><font color=red><h3 align=center>Login Failed.</h3></font>";
+    }
+}
+?>
--- login_logic.php
+++ login_logic.php
@@ -1,42 +1,9 @@
 <?php
-// https://codewithawa.com/posts/password-reset-system-in-php
 $errors = [];
 include('sanitize.php');
 include("dbcon2.php");
-$con = mysqli_connect("localhost", "root", "", "attendance");
-session_start();
-if (isset($_POST['Login'])) {
-    $username = sanitize(mysqli_real_escape_string($con, $_POST['username']));
-    $password = sanitize(mysqli_real_escape_string($con, $_POST['password']));
-    if (empty($userNAME)) array_push($errors, "Username or Email is required");
-    if (empty($password)) array_push($errors, "Password is required");
-    if (count($errors) == 0) {
-        $sql = "SELECT id,tag FROM user WHERE username = '$username' and password = '$password' ";
-        $result = mysqli_query($con, $sql);
-        if (!$result) {
-            printf("Error: %s\n", mysqli_error($con));
-            exit();
-        }
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        $count = mysqli_num_rows($result);
-        if ($count == 1) {
-            $_SESSION['state'] = 'active';
-            $_SESSION['name'] = $username;
-            if ($row['tag'] == 'student') {
-                header("Location: homestudent.php");
-            } else if($row['tag'] == 'teacher'){ 
-                header("Location: hometeacher.php");
-            }
-            else {
-                header("Location: home.php");
-            }
-        } else {
-            array_push($errors, "Wrong credentials");
-            echo "<br><font color=red><h3 align=center>Login Failed.</h3></font>";
-        }
-    }
-}
-else if (isset($_POST['reset-password'])) {
+if (isset($_POST['reset-password'])) {
+    $con = mysqli_connect("localhost", "root", "", "attendance");
     $uname = mysqli_real_escape_string($con, $_POST['uname']);
     // ensure that the user exists on our system
     $query = "SELECT username FROM user WHERE username='$uname'";
@@ -60,6 +27,7 @@ else if (isset($_POST['reset-password'])) {
   
   // ENTER A NEW PASSWORD
   if (isset($_POST['new_password'])) {
+    $con = mysqli_connect("localhost", "root", "", "attendance");
     $new_pass = mysqli_real_escape_string($con, $_POST['new_pass']);
     $new_pass_c = mysqli_real_escape_string($con, $_POST['new_pass_c']);
   
